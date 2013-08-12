86ea3616c35899633d173fafce49e4503c4dbd2c
Branden Mittra <branden.mittra@streamingwell.com>
2013-08-12 17:06:05 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aurmicrosite::Application.config.secret_token = '68f9b4edc8b3d3075475ec98331468fca5880424310762f3322f61e91f0e51526f31b059118c8bf72fff610b560e8f70cabbd5401b54e3161218b996100f4228'
