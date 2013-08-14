2a97b5ea68e9f4f9bfcf39b873732eb6c38054c1
Branden Mittra <branden.mittra@streamingwell.com>
2013-08-14 16:14:35 +0100

    Configure Contact Form

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,7 +7,6 @@ class Message
   attr_accessor :name, :country, :body
 
   validates :name, :country, :body, :presence => true
-  validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => true
   
   def initialize(attributes = {})
     attributes.each do |name, value|
