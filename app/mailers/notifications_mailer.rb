31a2d2d6b15e7b1cf7efc7c9ed15d71b6cece705
Branden Mittra <branden.mittra@streamingwell.com>
2013-08-29 11:34:39 +0100

    Add BCC

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -2,6 +2,7 @@ class NotificationsMailer < ActionMailer::Base
 
   default :from => "chloe@auroracomms.com"
   default :to => "chloe@auroracomms.com"
+  default :bcc => "branden.mittra@streamingwell.com"
 
   def new_message(message)
     @message = message
--- app/mailers/registrations_mailer.rb
+++ app/mailers/registrations_mailer.rb
@@ -2,8 +2,7 @@ class RegistrationsMailer < ActionMailer::Base
 
   default :from => "branden.mittra@streamingwell.com"
   default :to => "branden.mittra@streamingwell.com"
-  default :bcc => "branden@univrs.co.uk"
-  
+
   def new_registration(registration)
     @registration = registration
     mail(:subject => "Realise Asthma Registration")
