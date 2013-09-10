e8f2ae8a0340e41ad829a8466ad69961f90884cd
Branden Mittra <branden.mittra@streamingwell.com>
2013-09-10 17:18:01 +0100

    Add correct registration route

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -9,7 +9,7 @@ def new
     
     if @registration.valid?
       RegistrationsMailer.new_registration(@registration).deliver
-      redirect_to(welcome_path, :notice => "You have successfully registered. We'll send you a confirmation email shortly.")
+      redirect_to(live_path, :notice => "You have successfully registered. We'll send you a confirmation email shortly.")
 
     else
       redirect_to(root_path, :notice => "Please fill all fields and tick to confirm you are a journalist.")
