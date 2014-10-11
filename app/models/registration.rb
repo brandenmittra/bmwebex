8a91de14e93dbd69c5c10dfae41003521deee156
brandenmittra <branden@univrs.co.uk>
2014-10-11 18:48:59 +0100

    Fix domain block

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -8,11 +8,11 @@ class Registration
 
   validates :fname, :lname, :email, :job, :presence => true
 
-  INVALID_EMAILS = %w(virginmedia.com ee.co.uk plus.net aol.com aol.co.uk sky.com tiscali.co.uk btinternet.com gmail.com googlemail.com hotmail.co.uk hotmail.com live.co.uk yahoo.com yahoo.co.uk yahoo.gr yahoo.fr outlook.com outlook.co.uk icloud.com ymail.com mail.com bigstring.com talktalk.co.uk )
+  VALID_EMAILS = %w(bayer.com rockmedcomms.com streamingwell.com)
 
   validates_format_of :email,
-  :without => /#{INVALID_EMAILS.map{|a| Regexp.quote(a)}.join('|')}/,
-  :message => "Please register using your work email address."
+  :with => /#{VALID_EMAILS.map{|a| Regexp.quote(a)}.join('|')}/,
+  :message => "Please login using your work email address."
   
   def initialize(attributes = {})
     attributes.each do |name, value|
